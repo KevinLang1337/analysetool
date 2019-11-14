2e7d4b1b33ad9e4fcab79e39885894614327369f
Kevin <kevin.lang@uni-oldenburg.de>
2019-11-14 16:29:52 +0100

    Datepicker eingefügt; Rechte Spalte angepasst

---
+++ tool/static/js/datepicker.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.datepicker').datepicker({
+        format: "dd.mm.yyyy",
+        language: "ger",
+        changeMonth: true,
+        changeYear: true
+    });
+});

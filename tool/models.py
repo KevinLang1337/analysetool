e296eb7b0f2b20f9dca9ea81835f87c2c314e4d9
Christin2201 <christin.hilmer@uni-oldenburg.de>
2020-01-23 16:57:04 +0100

    Files löschen

--- tool/models.py
+++ tool/models.py
@@ -1,4 +1,7 @@
+
 from django.db import models
+
+
 
 class Document(models.Model):
     title = models.CharField(max_length=100, blank=True)
@@ -7,3 +10,6 @@ class Document(models.Model):
 
     def __str__(self):
         return self.title
+
+
+    
